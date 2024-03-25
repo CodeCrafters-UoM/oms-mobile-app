f2bcf9ef765ae3aff3e4aa4ae18e0d3945236cba
Abeysiri A.D.R.V <vishmika2adr@gmail.com>
2024-03-25 20:21:26 +0530

    create activation url

---
+++ app/src/main/java/com/example/deleever/ActivationURL.java
@@ -0,0 +1,14 @@
+package com.example.deleever;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivationURL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activation_url);
+    }
+}
