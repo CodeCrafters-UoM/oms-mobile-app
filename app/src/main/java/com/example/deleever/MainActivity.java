9a14e5c95908539bcbec676938ccbad0b476f77a
Anjana K.K <141270338+Kavindu822@users.noreply.github.com>
2024-03-25 19:23:37 +0530

    Product list updated

--- app/src/main/java/com/example/deleever/MainActivity.java
+++ app/src/main/java/com/example/deleever/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.deleever;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_home_product;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_home_product = findViewById(R.id.btn_home_report);
+
+        btn_home_product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent clicked_product_btn = new Intent(getApplicationContext(), product_list.class);
+                startActivity(clicked_product_btn);
+            }
+        });
     }
 }
