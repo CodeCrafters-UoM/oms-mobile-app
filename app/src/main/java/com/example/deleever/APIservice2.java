7349e3289674d0cae39552e2a2c7d28d48fecd60
Anjana K.K <141270338+Kavindu822@users.noreply.github.com>
2024-03-31 22:00:52 +0530

    call the api
    update product list page and connect to the api

---
+++ app/src/main/java/com/example/deleever/APIservice2.java
@@ -0,0 +1,26 @@
+package com.example.deleever;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface APIservice2 {
+
+    @GET("products")
+    Call<List<com.example.deleever.Product>> getProducts();
+
+
+    @GET("products/{productCode}")
+    Call<com.example.deleever.Product> getProductDetails(@Path("productCode") String productCode);
+
+    @DELETE("product/{productCode}")
+    Call<Void> deleteProduct(@Path("productCode") String productCode);
+
+    @PUT("products/{productCode}")
+    Call<Void> updateProduct(@Path("productCode") String productCode, @Body com.example.deleever.Product product);
+}
