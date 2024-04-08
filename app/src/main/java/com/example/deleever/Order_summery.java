2b6f49f2398a825acb1850297412ce9585a879c4
Abeysiri A.D.R.V <vishmika2adr@gmail.com>
2024-04-08 21:40:49 +0530

    get more details when click on customer details

--- app/src/main/java/com/example/deleever/Order_summery.java
+++ app/src/main/java/com/example/deleever/Order_summery.java
@@ -32,6 +32,7 @@ public class Order_summery extends AppCompatActivity {
         String Order_description_data = getIntent().getStringExtra("description");
         String customer_address_data = getIntent().getStringExtra("contact");
         String customer_contact_data = getIntent().getStringExtra("address");
+        String order_status_data = getIntent().getStringExtra("status");
 
         TextView customer_name = findViewById(R.id.customer_name_data);
         customer_name.setText(customer_name_data);
@@ -54,6 +55,25 @@ public class Order_summery extends AppCompatActivity {
         TextView Order_id = findViewById(R.id.Order_id_data);
         Order_id.setText(Order_id_data);
 
+
+        TextView order_status = findViewById(R.id.status);
+        order_status.setText(order_status_data);
+
+
+        TextView Customer_details = findViewById(R.id.Customer_details);
+        Customer_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent click_Customer_details = new Intent(getApplicationContext(),Customer_details_screen.class);
+                click_Customer_details.putExtra("name",customer_name_data);
+                click_Customer_details.putExtra("address",customer_address_data);
+                click_Customer_details.putExtra("contact",customer_contact_data);
+                startActivity(click_Customer_details);
+
+
+
+            }
+        });
     }
 }
 
