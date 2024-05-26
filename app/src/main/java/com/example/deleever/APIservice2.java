70b0ee37cf645161495e7714bd60b48fa81b40c3
Anjana K.K <141270338+Kavindu822@users.noreply.github.com>
2024-05-26 20:02:14 +0530

    cl api and modify login page

--- app/src/main/java/com/example/deleever/APIservice2.java
+++ app/src/main/java/com/example/deleever/APIservice2.java
@@ -6,10 +6,16 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface APIservice2 {
+
+    @POST("login")
+    Call<com.example.deleever.LoginResponse> LogIn(
+            @Body com.example.deleever.LoginRequest loginRequest
+    );
 
     @GET("products")
     Call<List<com.example.deleever.Product>> getProducts();
